85fc841a322da041a479e06724fd5b8f648acbb9
Felix Uhl <felix.uhl@netlight.com>
2023-07-25 19:01:43 +0200

    Pull demo setup into separate file

---
+++ demo-setup.sh
@@ -0,0 +1,23 @@
+# Include the magic
+. ./demo-magic/demo-magic.sh
+
+# Create separate demo profile to avoid affecting user profile
+DEMO_PROFILE=/tmp/demo-profile
+
+# Restore user profile on exit
+RESTORE_NIX_USER_PROFILE=$(readlink ~/.nix-profile)
+function exit_hook {
+    nix-env --switch-profile "$RESTORE_NIX_USER_PROFILE"
+    rm -rf "$DEMO_PROFILE"
+}
+trap exit_hook EXIT
+
+# Switch to demo profile only after ensuring the user profile will be restored
+nix-env --switch-profile "$DEMO_PROFILE"
+
+# Configure demo-magic
+# TYPE_SPEED=0
+PROMPT_COLOR="$(tput setab 4)$(tput setaf 15)"
+RESET_STYLE="$(tput sgr0)"
+TRIANGLE_COLOR="$RESET_STYLE$(tput setaf 4)"
+DEMO_PROMPT="$PROMPT_COLOR \w $TRIANGLE_COLOR"" $RESET_STYLE"
--- demo.sh
+++ demo.sh
@@ -1,28 +1,6 @@
 #!/usr/bin/env bash
 
-# Include the magic
-. ./demo-magic/demo-magic.sh
-
-# Create separate demo profile to avoid affecting user profile
-DEMO_PROFILE=/tmp/demo-profile
-
-# Restore user profile on exit
-RESTORE_NIX_USER_PROFILE=$(readlink ~/.nix-profile)
-function exit_hook {
-    nix-env --switch-profile "$RESTORE_NIX_USER_PROFILE"
-    rm -rf "$DEMO_PROFILE"
-}
-trap exit_hook EXIT
-
-# Switch to demo profile only after ensuring the user profile will be restored
-nix-env --switch-profile "$DEMO_PROFILE"
-
-# Configure demo-magic
-# TYPE_SPEED=0
-PROMPT_COLOR="$(tput setab 4)$(tput setaf 15)"
-TRIANGLE_COLOR="$(tput setab 8)$(tput setaf 4)"
-RESET_STYLE="$(tput sgr0)"
-DEMO_PROMPT="$PROMPT_COLOR \w $TRIANGLE_COLOR"" $RESET_STYLE"
+. demo-setup.sh
 
 # Hide the evidence
 clear
