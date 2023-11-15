eeb96cf7587219f5691ab853621ad5eddfbdd033
Felix Uhl <github@mail.felix-uhl.de>
2023-11-15 21:58:55 +0100

    Try mdx-deck
    I didn't even get to integrating xterm. mdx-deck has been abandoned
    since 2021, and only works with nodejs 16. This seems like way too much
    of a hassle if I want to use any modern version of React or xterm.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,9 @@
 {
   description = "Presentation of Nix functionality and concepts.";
 
+  # As mdx-deck is a defunct project, we need to use an old version of nodejs, otherwise it doesn't work
+  # This commit contains nodejs 16.6.2. See https://lazamar.co.uk/nix-versions/?channel=nixpkgs-unstable&package=nodejs
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/c8e344196154514112c938f2814e809b1ca82da1";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -9,7 +12,7 @@
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            nodejs_20
+            nodejs
           ];
         };
       });
