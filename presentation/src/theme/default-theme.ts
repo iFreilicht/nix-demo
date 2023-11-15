581db440c4336c24dcab55100fb58dcd441f6b97
Felix Uhl <github@mail.felix-uhl.de>
2023-11-15 23:04:51 +0100

    Add spectacle and first two slides

---
+++ presentation/src/theme/default-theme.ts
@@ -0,0 +1,16 @@
+import { SpectacleThemeOverrides } from "spectacle";
+
+export const defaultTheme: SpectacleThemeOverrides = {
+  colors: {
+    primary: "white",
+    secondary: "#eee",
+    tertiary: "#111", // Default background color
+  },
+  fonts: {
+    header: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+  },
+  fontSizes: {
+    h1: "72px",
+    h2: "64px",
+  },
+};
