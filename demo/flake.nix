896919eacc805fa209b04de774ec66cbb031bb5d
Felix Uhl <github@mail.felix-uhl.de>
2023-12-14 15:04:22 +0100

    Upgrade nix to 2.18

--- demo/flake.nix
+++ demo/flake.nix
@@ -2,6 +2,7 @@
   description = "A scripted in-shell demo of Nix.";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -13,7 +14,7 @@
 
           packages = with pkgs; [ # _
             # Use a locked version of nix independent of the system/user-installed one
-            nixVersions.nix_2_16 # TODO: Upgrade to 2_17 or 2_18 once released
+            nixVersions.nix_2_18
             # Basic shell environment
             coreutils
             ncurses
